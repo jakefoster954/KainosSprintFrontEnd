25faeb55b2b64772f5b3012f703c89c50b2bbffc
Matthew Doherty <matthew.doherty1@kainos.com>
2021-07-14 15:31:25 +0100

    Add default photot if no capability lead photo is present, update endpoint for capability lead data, move css file out of components folder

--- src/servcies/jobService.js
+++ src/servcies/jobService.js
@@ -9,10 +9,10 @@ export function getJobRole(jobName) {
 }
 
 export function getCapabilities() {
-	console.log("GetCapabilities() is running")
+	console.log('GetCapabilities() is running')
 	return http.get('http://localhost:8080/api/getCapabilityLeads')
 }
 
 export function getCapabilityLead(leadName) {
-	return http.get(`http://localhost:8080/api/getCapabilityLeadData/${leadName}`)
+	return http.get(`http://localhost:8080/api/getCapabilityData/${leadName}`)
 }
