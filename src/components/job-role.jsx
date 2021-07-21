16ed355d315e928045df54dcbfa83acc31956d2a
Barry Bell <gavin.bell@kainos.com>
2021-07-21 16:07:05 +0100

    Display training information on job role page

--- src/components/job-role.jsx
+++ src/components/job-role.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import { deleteJobRole, getJobRole } from '../servcies/jobService'
+import { deleteJobRole, getJobRole, getTrainingData } from '../servcies/jobService'
 
 class JobRole extends Component {
 	state = {
 		jobRole: {},
+		trainingCourses: []
 	}
 
 	async retreiveJobRole() {
@@ -11,8 +12,16 @@ class JobRole extends Component {
 		this.setState({ jobRole })
 	}
 
+	async retrieveTraining() {
+		const { jobRole } = this.state
+		const { data: trainingCourses } = await getTrainingData(jobRole.bandName)
+		this.setState({ trainingCourses })
+		console.log(trainingCourses)
+	}
+
 	async componentDidMount() {
 		await this.retreiveJobRole()
+		await this.retrieveTraining()
 	}
 
 	handleDelete = async (jobName) => {
@@ -29,7 +38,7 @@ class JobRole extends Component {
 	}
 
 	render() {
-		const { jobRole } = this.state
+		const { jobRole, trainingCourses } = this.state
 		const { user } = this.props
 		return (
 			<div className='row mt-3 d-flex justify-content-center'>
@@ -38,7 +47,7 @@ class JobRole extends Component {
 						<h1 id='jobHeader'>{jobRole.jobName}</h1>
 						<h3 id='capabilityHeader'>Capability - {jobRole.capabilityName}</h3>
 						<h3 id='bandLevelHeader'>Band Level - {jobRole.bandName}</h3>
-						{user === "ADMIN" && (
+						{user === "ADMI" && (
 							<a class="btn btn-danger text-white" onClick={() => this.handleDelete(jobRole.jobName)}>Delete Job Role</a>
 						)}
 					</div>
@@ -50,15 +59,37 @@ class JobRole extends Component {
 							</div>
 						</div>
 					</div>
-					<div className='card-body'>
-						Find out more{' '}
-						<a
+					<div className='card-body text-center'>
+						<a class="btn btn-primary text-white"
 							href={`${jobRole.jobUrl}`}
 							target='_blank'
-							rel='noopener noreferrer'
+							rel='noopener noreferrer'>Find out more</a>
+					</div>
+					<div className='p-5'>
+						<h2 className='pb-3'>Relevant Training (Band - {jobRole.bandName})</h2>
+						<table
+							className='table table-striped table-bordered text-center pt-3'
+							id='trainingTable'
 						>
-							here
-						</a>
+							<thead>
+								<tr>
+									<th>Training</th>
+									<th>Link</th>
+								</tr>
+							</thead>
+							<tbody>
+								{trainingCourses.map((training) => (
+									<tr className='tableBody' key={training.trainingName}>
+										<td>{training.trainingName}</td>
+										<td><a
+											className='btn btn-secondary'
+											href={`${training.trainingLink}`}
+											target='_blank'
+											rel='noopener noreferrer'>Find out more</a></td>
+									</tr>
+								))}
+							</tbody>
+						</table>
 					</div>
 				</div>
 			</div>
